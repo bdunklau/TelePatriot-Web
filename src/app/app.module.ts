f72665ca4d6be7a4d30eb9923c86d0c11723e8db
Brent Dunklau <bdunklau@yahoo.com>
2019-10-08 14:17:07 -0500

    When trying to: ng build --prod,  got this error: ERROR in 'router-outlet' is not a known element: 1. If 'router-outlet' is an Angular component, then verify that it is part of this module. 2. If 'router-outlet' is a Web Component then add 'CUSTOM_ELEMENTS_SCHEMA' to the '@NgModule.schemas' of this component to suppress this message. (" [ERROR ->]<router-outlet></router-outlet> ")
    This commit fixed this error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoInvitationComponent } from './video-invitation/video-invitation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoInvitationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
