0e9f5f3597408a41804e0630b9265a5364508d12
Brent Dunklau <bdunklau@yahoo.com>
2019-11-12 11:41:57 -0600

    This component allows us to grant/revoke the permission to initiate video calls.  The role name is "Video Creator" and belongs in the roles node under each user

---
+++ src/app/video-creators/video-creators.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user/user.service';
+import * as _ from 'lodash';
+
+
+@Component({
+  selector: 'app-video-creators',
+  templateUrl: './video-creators.component.html',
+  styleUrls: ['./video-creators.component.css']
+})
+export class VideoCreatorsComponent implements OnInit {
+
+    cbidValue: number;
+    user: {citizen_builder_id?: number, name?: string, state_abbrev?: string, roles?: string[]};
+
+
+    constructor(private userService: UserService) { }
+
+    ngOnInit() {
+    }
+
+
+    // makes the person a Video Creator
+    async onSubmit() {
+        console.log('parseInt: this.cbidValue = ', this.cbidValue);
+        await this.userService.addRole(this.cbidValue, "Video Creator");
+        this.user = await this.userService.getUser(this.cbidValue);
+        console.log('this.user = ', this.user);
+    }
+
+    async removeVideoCreatorRole() {
+        await this.userService.removeRole(this.cbidValue, "Video Creator");
+        this.user = await this.userService.getUser(this.cbidValue);
+    }
+
+}
