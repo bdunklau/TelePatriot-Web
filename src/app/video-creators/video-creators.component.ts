68aa55bd8d59e5c26a1e87a934cc5040309df614
Brent Dunklau <bdunklau@yahoo.com>
2019-11-20 20:56:55 -0600

    import { take }

--- src/app/video-creators/video-creators.component.ts
+++ src/app/video-creators/video-creators.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user/user.service';
 import * as _ from 'lodash';
+import { take } from 'rxjs/operators';
 
 
 @Component({
